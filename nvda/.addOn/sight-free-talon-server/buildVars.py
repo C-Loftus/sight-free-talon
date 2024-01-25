c8fc8ca2362d70a747dc62fd933d13ef43282e2d
Colton <70598503+C-Loftus@users.noreply.github.com>
2024-01-25 16:35:52 -0500

    addon version update

--- nvda/.addOn/sight-free-talon-server/buildVars.py
+++ nvda/.addOn/sight-free-talon-server/buildVars.py
@@ -23,8 +23,7 @@ addon_info = {
 	"addon_summary": _("Sight-Free-Talon"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description": _("""Description for the add-on.
-It can span multiple lines."""),
+	"addon_description": _("""Server to Communicate With Talon Through the Screen Reader"""),
 	# version
 	"addon_version": "1.0",
 	# Author(s)
@@ -36,9 +35,9 @@ It can span multiple lines."""),
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": None,
-	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": None,
+	"addon_minimumNVDAVersion" : "2019.3",
+	# Last NVDA version supported/tested
+	"addon_lastTestedNVDAVersion" : "2023.3.0",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!

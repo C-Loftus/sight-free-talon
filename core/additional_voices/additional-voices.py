9cd6a8021b1560781aadd622fb83b70b6f732a3b
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2024-02-15 16:33:18 -0500

    change debug default

--- core/additional_voices/additional-voices.py
+++ core/additional_voices/additional-voices.py
@@ -5,35 +5,7 @@ mod = Module()
 @mod.action_class
 class Actions:
 
-    def openai_tts(text: str):
-        """text to speech with openai"""
-        url = "https://api.openai.com/v1/audio/speech"
-        try:
-            TOKEN = os.environ["OPENAI_API_KEY"]
-        except:
-            actions.user.tts("GPT Failure: No API Key")   
-            return
-        
-        headers = {
-            "Authorization": f"Bearer {TOKEN}",
-            "Content-Type": "application/json",
-        }
-
-        data = {
-            "model": "tts-1",
-            "input": text,
-            "voice": "alloy",
-        }
-
-        response = requests.post(url, headers=headers, data=json.dumps(data))
-
-        if response.status_code == 200:
-            date = time.strftime("%Y-%m-%d-%H-%M-%S")
-            with open(f"openai-speech-{date}.mp3", "wb") as f:
-                f.write(response.content)
-            print("Speech successfully saved to openai-speech.mp3")
-        else:
-            print(f"Error: {response.status_code}, {response.text}")
+    
 
 
     def elevenlabs_tts(text: str):
